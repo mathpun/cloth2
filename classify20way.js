38fc35e8fc6dcd2ed5eb6f663831282c7ca33fed
mathpun <eulersmail@gmail.com>
2016-10-26 15:40:54 -0400

    add ability to random demos
    :)

--- classify20way.js
+++ classify20way.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 	Tool_id = 2;
 	Frack_id = 4;
 
+
 	//took out line 18,19 added line 16, shit, turns out you need spec.list_condition 
 	//var spec = {};
 	var spec = {};
@@ -23,12 +24,33 @@ $(document).ready(function() {
 	var data = {};
 	data.imglist_test = getlist_test(condition,ntask,nway);
 	data.imglist_list_train = getlist_train(condition,ntask,nway);
-	data.imglist_demo = getlist_test_demo(Plant_id,Shell_id,Tool_id,Frack_id);
-	data.imglist_list_demo = getlist_train_demo(nway);
+	var inds = shuffle([0,1,2,3]);
+	data.imglist_demo = getlist_test_demo(Plant_id,Shell_id,Tool_id,Frack_id,inds);
+	data.imglist_list_demo = getlist_train_demo(nway,inds);
 	task.load_images(data);
 	console.log("imglist_test:"+data.imglist_test);
 	console.log("imglist_train:"+data.imglist_list_train);
 });
+
+// shuffle I found on stack overflow
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 // randomly choose a "test" image for each trial
 var getlist_test = function (type,ntask,nway) {
@@ -57,41 +79,41 @@ var getlist_train = function (type,ntask,nway) {
 };
 
 // Get just the two test images for the demo
-var getlist_test_demo = function (Plant_id,Shell_id,Tool_id,Frack_id) {
+var getlist_test_demo = function (Plant_id,Shell_id,Tool_id,Frack_id,r) {
 	var list = new Array();
 	var dname = 'images_classif_demo/';
 //  	list[0] = dname + 'latin_test' + latin_id + '.png';
-	list[0] = dname + 'Plant_test' + Plant_id + '.png';
-	list[1] = dname + 'Shell_test' + Shell_id + '.png';
-	list[2] = dname + 'Tool_test' + Tool_id + '.png';
-	list[3] = dname + 'Frack_test' + Frack_id + '.png';
+	list[r[0]] = dname + 'Plant_test' + Plant_id + '.png';
+	list[r[1]] = dname + 'Shell_test' + Shell_id + '.png';
+	list[r[2]] = dname + 'Tool_test' + Tool_id + '.png';
+	list[r[3]] = dname + 'Frack_test' + Frack_id + '.png';
 	return list;
 };
 
 // Get two lists of training images
-var getlist_train_demo = function (nway) {
+var getlist_train_demo = function (nway,r) {
 	var list = new Array();
 	var dname = 'images_classif_demo/';
 //
 
-	list[0] = new Array();
+	list[r[0]] = new Array();
 	for (var c=1; c <= nway; c++ ) {
-  		list[0][c-1] = dname + 'Plant_train' + c + '.png';
+  		list[r[0]][c-1] = dname + 'Plant_train' + c + '.png';
     }
 
-	list[1] = new Array();
+	list[r[1]] = new Array();
 	for (var c=1; c <= nway; c++ ) {
-	  	list[1][c-1] = dname + 'Shell_train' + c + '.png';
+	  	list[r[1]][c-1] = dname + 'Shell_train' + c + '.png';
 	  }
 
-	list[2] = new Array();
+	list[r[2]] = new Array();
 	for (var c=1; c <= nway; c++ ) {
-	  	list[2][c-1] = dname + 'Tool_train' + c + '.png';
+	  	list[r[2]][c-1] = dname + 'Tool_train' + c + '.png';
 	  }
 
-	list[3] = new Array();
+	list[r[3]] = new Array();
 	for (var c=1; c <= nway; c++ ) {
-	  	list[3][c-1] = dname + 'Frack_train' + c + '.png';
+	  	list[r[3]][c-1] = dname + 'Frack_train' + c + '.png';
 	  }
 
 
